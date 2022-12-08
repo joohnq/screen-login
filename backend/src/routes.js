45f9400eb46e50107a40d68afb66c9ec7e07e7be
João Henrique <joaohenriquess3287@gmail.com>
2022-12-08 09:57:17 -0300

    fix and feat: verify inputs and fix delete method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,39 +3,59 @@ const routes = express.Router();
 const User = require("./database/models/User");
 
 routes.get("/users", async (req, res) => {
-  const users = await User.find({});
+  try {
+    const users = await User.find({});
 
-  if (users == "") {
-    return res.status(404).json({ msg: "Não há usuários no banco de dados" });
+    if (users == "") {
+      return res.status(404).json({ msg: "Não há usuários no banco de dados" });
+    }
+
+    if (users == null || users == undefined) {
+      return res
+        .status(500)
+        .json({ msg: "ERROR | Ocorreu um erro no banco de dados" });
+    }
+
+    return res.status(404).json(users);
+  } catch (error) {
+    console.log(error);
   }
-
-  if (users == null || users == undefined) {
-    return res
-      .status(500)
-      .json({ msg: "ERROR | Ocorreu um erro no banco de dados" });
-  }
-
-  return res.status(404).json(users);
 });
 
 routes.post("/users", async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const userCreated = await User.create({
-    name: name,
-    email: email,
-    password: password,
-  });
+    if (!name && !email && !password) {
+      return res.status(404).json({ msg: "Complete todos os campos" });
+    } else if (!name) {
+      return res.status(404).json({ msg: "Complete o campo de Nome" });
+    } else if (!email) {
+      return res.status(404).json({ msg: "Complete o campo de EMAIL" });
+    } else if (!password) {
+      return res.status(404).json({ msg: "Complete o campo de SENHA" });
+    } else {
+      const userCreated = await User.create({
+        name: name,
+        email: email,
+        password: password,
+      });
 
-  res.status(200).json(userCreated);
+      res.status(200).json(userCreated);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-routes.delete("users/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const userDeleted = await User.findByIdAndDelete(id);
-  console.log(userDeleted);
-  res.status(400).json(userDeleted);
+routes.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userDeleted = await User.findByIdAndDelete(id);
+    res.status(400).json(userDeleted);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = routes;
