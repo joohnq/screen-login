41f15d149c3d508dacf1ab4080c3ad3b26fcf140
João Henrique <joaohenriquess3287@gmail.com>
2022-12-08 09:24:17 -0300

    feat: Get and Post users on database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
+const { json } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
-require('./database/config/dbConfig')
+const routes = require("./routes");
+require("./database/config/dbConfig");
 
-app.get("/", (req, res) => res.json({ msg: "Success" }));
+app.use(express.json())
+app.use(routes);
+
+app.get("/", (req, res) => res.json({ msg: "Home" }));
 app.listen(port, () => console.log(`Rodando na porta: ${port}!`));
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,2 +1,41 @@
-// const express = require('express')
-// const router = express.Router()
+const express = require("express");
+const routes = express.Router();
+const User = require("./database/models/User");
+
+routes.get("/users", async (req, res) => {
+  const users = await User.find({});
+
+  if (users == "") {
+    return res.status(404).json({ msg: "Não há usuários no banco de dados" });
+  }
+
+  if (users == null || users == undefined) {
+    return res
+      .status(500)
+      .json({ msg: "ERROR | Ocorreu um erro no banco de dados" });
+  }
+
+  return res.status(404).json(users);
+});
+
+routes.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userCreated = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  res.status(200).json(userCreated);
+});
+
+routes.delete("users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const userDeleted = await User.findByIdAndDelete(id);
+  console.log(userDeleted);
+  res.status(400).json(userDeleted);
+});
+
+module.exports = routes;
